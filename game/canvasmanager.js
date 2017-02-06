dbcc069beb7c8ee9890a5ab581ce8eb88b2f1931
Ashton <aboyd52501@gmail.com>
2017-02-06 17:50:26 -0600

    setup game for github

---
+++ game/canvasmanager.js
@@ -0,0 +1,115 @@
+function initCanvas( self, frameRate=24, w=1000, h=900 ) {
+	
+	console.log( self );
+	
+	var canvas = document.getElementById("gravity-sim");
+	var context = canvas.getContext("2d");
+	context.width = w; context.height = h;
+	canvas.width = w; canvas.height = h;
+	
+	var viewOffset = new Vector();
+	var viewZoom = 1;
+	
+	var particles = new ParticlePopulation();
+	
+	var gravityConstant = 1;
+	var timeStep = 10/frameRate;
+	var viewOffset = particles.massCenter;
+	var mass = 1;
+	
+	var pPos = new Vector();
+	var mouseDown = false;
+	
+	var mouseX; var mouseY;
+	
+	function disk( position, radius, amount ) {
+		for( var i = 0; i < amount; i++ ) {
+			var polarOffset = new VectorPolar(Math.PI*2*(i/amount), Math.random()*(radius*3/4) + radius/4 );
+			var newPos = Vector.Add( position, VectorPolar.ToRectangular( polarOffset ) );
+			var velDir = VectorPolar.ToRectangular( new VectorPolar( polarOffset.theta + Math.PI/2, 1 ) );
+			particles.AddParticle( new Particle(
+				newPos,
+				Vector.Mul( velDir, gravityConstant * Math.sqrt( polarOffset.distance ) ),
+				mass
+			) );
+		}
+	}
+	
+	function fillScreen( amount, velfxn, massfxn ) {
+		for( var i = 0; i < amount; i++ ) {
+			particles.AddParticle( new Particle(
+				new Vector( Math.random()*w, Math.random() * h ),
+				velfxn( i ),
+				massfxn( i )
+			) );
+		}
+	}
+	
+	function ScreenToWorld( vec ) {
+		var mid = new Vector( w/2, h/2 );
+		var rel = Vector.Sub( mid, vec );
+		var world = Vector.Add( Vector.Div( rel, viewZoom ), viewOffset );
+		return world;
+	}
+	
+	function Draw() {
+		viewOffset = particles.massCenter;
+		context.clearRect( 0, 0, w, h );
+		context.fillStyle="black"; context.fillRect( 0, 0, w, h );
+		particles.StepParticles( gravityConstant, timeStep );
+		particles.DrawParticles( context, viewOffset, viewZoom );
+		if( mouseDown ) {
+			context.beginPath();
+			context.moveTo( pPos.x, pPos.y );
+			context.lineTo( mouseX, mouseY );
+			context.strokeStyle = "#FFFFFF";
+			context.stroke();
+		};
+		$("#mass-display")[0].innerHTML = "Mass: " + Math.round(mass*10)/10;
+	}
+	
+	$(document).keypress( function( key ) {
+		key = key.keyCode || key.which;
+		//console.log( key );
+		if( key === 43 ) { viewZoom *= 1.1; }
+		if( key === 45 ) { viewZoom /= 1.1; }
+		if( key === 57 ) { mass *= Math.sqrt(2); }
+		if( key === 54 ) { mass /= Math.sqrt(2); }
+		if( key === 32 ) {
+			disk(
+				ScreenToWorld( new Vector( mouseX, mouseY ) ),
+				128*Math.cbrt(mass), 64
+			); }
+	} );
+	
+	$("#gravity-sim").click( function( click ) {
+		var newPos = ScreenToWorld( new Vector( click.offsetX, click.offsetY ) );
+		var oldPos = ScreenToWorld( pPos );
+		var pVel = Vector.Sub( newPos, oldPos );
+		particles.AddParticle( new Particle(
+			oldPos,
+			Vector.Div( pVel, 5 ),
+			mass
+		) );
+		mouseDown = false;
+	} );
+	
+	$("#gravity-sim").mousedown( function( mouse ) {
+		mouseDown = true;
+		pPos = new Vector( mouse.offsetX, mouse.offsetY );
+	} );
+	
+	$("#gravity-sim").mousemove( function( mouse ) {
+		mouseX = mouse.offsetX; mouseY = mouse.offsetY;
+	} );
+	
+	/* fillScreen( 128,
+	function( x ) { return new Vector( Math.random()*30-15, Math.random()*30-15 ); },
+	function( x ) { return Math.random()*1000; }
+	); */
+	
+	setInterval( Draw, 1000/frameRate );
+	
+}
+
+document.addEventListener( "DOMContentLoaded", initCanvas );
---
+++ game/gravitysim.js
@@ -0,0 +1,121 @@
+class Particle {
+	constructor( pos, vel, mass ) {
+		this.position = pos;
+		this.velocity = vel;
+		this.mass = mass;
+		this.type = "Particle";
+		this.color = new ColorHSL();
+	}
+	get radius() {
+		return Math.pow( this.mass, 1/3 );
+	}
+	Draw( ctx, offset, zoom ) {
+		var midVec = new Vector( ctx.width/2, ctx.height/2 );
+		var view = Vector.Mul( Vector.Sub( offset, this.position ), zoom );
+		view = Vector.Add( view, midVec );
+		ctx.beginPath();
+		ctx.arc(
+			view.x,
+			view.y,
+			this.radius * zoom, 0, 2*Math.PI
+		);
+		ctx.fillStyle = ColorHSL.toString( this.color );
+		ctx.fill();
+		//ctx.stroke();
+	}
+	// actually applyacceleration but that isn't as catchy
+	ApplyForce( force ) {
+		this.velocity = Vector.Add( this.velocity, force );
+	}
+	PhysicsStep( timeMul ) {
+		this.position = Vector.Add( this.position, Vector.Mul( this.velocity, timeMul ) );
+	}
+	static GravityAccel( p1, p2 ) {
+		var dist = Vector.Dist( p1.position, p2.position );
+		//dist = Vector.Mul( dist, 0.1 );
+		var dir = Vector.Norm( Vector.Sub( p2.position, p1.position ) );
+		var accel = p2.mass / Math.pow( dist, 2 );
+		if( isNaN( accel ) ) { throw( "gravityaccel not a number error" ); }
+		return Vector.Mul( dir, accel );
+	}
+	static CheckCollide( p1, p2 ) {
+		var dist = Vector.Dist( p1.position, p2.position );
+		return dist < ( p1.radius + p2.radius );
+	}
+}
+
+class ParticlePopulation {
+	constructor() {
+		this.Population = [];
+		this.type = "ParticlePopulation";
+	}
+	AddParticle( p ) {
+		this.Population.push( p );
+	}
+	RemoveParticle( index ) {
+		this.Population.splice( index, 1 );
+	}
+	NetGravityAccel( p ) {
+		var net = new Vector();
+		this.Population.forEach( function( otherp ) {
+			if( otherp === p ) { return; }
+			net = Vector.Add( net, Particle.GravityAccel( p, otherp ) );
+		} );
+		return net;
+	}
+	ApplyGravityForces( g ) {
+		var pop = this;
+		pop.Population.forEach( function( particle )  {
+			var force = Vector.Mul( pop.NetGravityAccel( particle ), g );
+			particle.ApplyForce( force );
+		} );
+	}
+	RunCollisions() {
+		var pop = this;
+		for( var i = 0; i < pop.Population.length; i++ ) {
+			var p1 = pop.Population[i];
+			for( var j = 0; j < pop.Population.length; j++ ) {
+				var p2 = pop.Population[j]
+				if( !(p1 === p2 ) && Particle.CheckCollide( p1, p2 ) ) {
+					var totalMass = p1.mass + p2.mass
+					var newVel = Vector.Add( Vector.Mul( p1.velocity, p1.mass/totalMass ), Vector.Mul( p2.velocity, p2.mass/totalMass ) );
+					if( p1.mass >= p2.mass ) {
+						p1.mass = totalMass;
+						p1.velocity = newVel;
+						pop.RemoveParticle( j );
+					} else {
+						p2.mass = totalMass;
+						p2.velocity = newVel;
+						pop.RemoveParticle( i );
+					}
+				}
+			}
+		}
+	}
+	StepParticles( g, timeMul ) {
+		var gmul = g * timeMul;
+		this.ApplyGravityForces( gmul );
+		this.RunCollisions();
+		this.Population.forEach( function( particle ) {
+			particle.PhysicsStep( timeMul );
+		} );
+	}
+	DrawParticles( ctx, offset, zoom ) {
+		this.Population.forEach( function( particle ) {
+			particle.Draw( ctx, offset, zoom );
+		} );
+	}
+	get totalMass() {
+		var totalMass = 0;
+		this.Population.forEach( function( particle ) { totalMass += particle.mass; } );
+		return totalMass;
+	}
+	get massCenter() {
+		var avg = new Vector();
+		var totalMass = this.totalMass;
+		this.Population.forEach( function( particle ) {
+			avg = Vector.Add( avg, Vector.Mul( particle.position, particle.mass/totalMass ) );
+		} );
+		return avg;
+	}
+}
